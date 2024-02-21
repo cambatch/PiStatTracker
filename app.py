e169bbcd3532c9da32196e40c97c5d2bdbb9c6c2
cambatch <cameron.batchelor22@gmail.com>
2024-02-20 20:15:15 -0800

    Add flask starter code

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
+
+
+# These are just here to get something to show up for now...
+# This will probable change
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('displaySettings.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
