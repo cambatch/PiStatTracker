d097429cf33fcdd1ccd8a34d04434d046c982573
cambatch <95779065+cambatch@users.noreply.github.com>
2024-02-20 20:16:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__, static_folder='app/static', template_folder='app/templates
 
 
 # These are just here to get something to show up for now...
-# This will probable change
 @app.route('/')
 def home():
     return render_template('index.html')
